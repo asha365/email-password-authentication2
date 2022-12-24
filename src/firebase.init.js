4f69a76563b145517a5e55005383df3ddefb6c40
Asha <asha.engrbd@gmail.com>
2022-12-24 22:03:09 +0600

    This project uses form import react bootstrap. and add eventHandler in input field email and password. import from firebase/auth CreateUserEmailAndPassword and create 2 state and set for email and password

---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCL6ixHFJgMZY3wd3ZiTnhFAuTxqo-qkgk",
+    authDomain: "email-password-authentic-b94cd.firebaseapp.com",
+    projectId: "email-password-authentic-b94cd",
+    storageBucket: "email-password-authentic-b94cd.appspot.com",
+    messagingSenderId: "89220331180",
+    appId: "1:89220331180:web:c0b735983487f7f7295109"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
